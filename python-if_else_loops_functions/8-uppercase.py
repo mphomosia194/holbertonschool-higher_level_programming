a8879fd2914d055ce808394bc0cd6f7cf3616040
mphomosia194 <mphomosia194@gmail.com>
2026-06-01 11:03:50 +0200

    Fix task 8 print count

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,7 @@
 
 def uppercase(str):
     for c in str:
-        if 97 <= ord(c) <= 122:
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{}".format(c), end="")
-    print("")
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print()
