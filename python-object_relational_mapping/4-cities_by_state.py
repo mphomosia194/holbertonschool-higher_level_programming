94c0b0d11cf90ec0bbac844a7cfa3a342d98fd6c
mphomosia194 <mphomosia194@gmail.com>
2026-06-02 15:21:56 +0200

    Task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Lists all cities with their corresponding state."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+        """
+    )
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
