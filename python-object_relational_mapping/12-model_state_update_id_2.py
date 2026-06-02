960ca764021c14d13614bf1bfdb737ae5626a44c
mphomosia194 <mphomosia194@gmail.com>
2026-06-02 15:37:03 +0200

    Object relational tasks

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Changes the name of State object with id=2 to New Mexico."""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state is not None:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Lists all City objects with their corresponding State."""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = session.query(City, State)\
+                     .filter(City.state_id == State.id)\
+                     .order_by(City.id)\
+                     .all()
+
+    for city, state in results:
+        print("{}: ({}) {}".format(
+            state.name,
+            city.id,
+            city.name
+        ))
+
+    session.close()
