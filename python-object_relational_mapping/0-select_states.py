2d1312a5a6142b251476d038e0626cf029a0c1ed
mphomosia194 <mphomosia194@gmail.com>
2026-06-02 12:07:59 +0200

    Corrections

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa."""
+"""Lists all states from a MySQL database."""
 
 import MySQLdb
 import sys
@@ -11,14 +11,15 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    for row in cur.fetchall():
+    for row in cursor.fetchall():
         print(row)
 
-    cur.close()
+    cursor.close()
     db.close()
