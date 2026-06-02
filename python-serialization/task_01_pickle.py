d169458ad0d8be06a974f40c1e52b8dab6a79d36
mphomosia194 <mphomosia194@gmail.com>
2026-06-02 10:51:17 +0200

    corrections

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -8,31 +8,30 @@ class CustomObject:
     """Represents a custom object."""
 
     def __init__(self, name, age, is_student):
-        """Initialize a CustomObject instance."""
+        """Initialize a custom object."""
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """Display the object's attributes."""
+        """Display object attributes."""
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """Serialize the object and save it to a file."""
+        """Serialize the object to a file."""
         try:
             with open(filename, "wb") as file:
                 pickle.dump(self, file)
-        except (OSError, pickle.PickleError):
+        except Exception:
             return None
 
     @classmethod
     def deserialize(cls, filename):
-        """Deserialize an object from a file and return it."""
+        """Deserialize an object from a file."""
         try:
             with open(filename, "rb") as file:
                 return pickle.load(file)
-        except (FileNotFoundError, OSError,
-                pickle.PickleError, EOFError):
+        except Exception:
             return None
